68184f287fe0545c593773b33d44a5f0803e9a0c
Aayush Raj <144894045+AayushCoderBoy@users.noreply.github.com>
2024-11-30 14:42:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname,'public')));
-
-const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 
 // PostgreSQL connection
