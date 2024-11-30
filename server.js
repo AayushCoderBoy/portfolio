7d0073d99127f246f40b22dbfc236fb6326bcddf
Aayush Raj <144894045+AayushCoderBoy@users.noreply.github.com>
2024-11-30 14:36:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -192,6 +192,10 @@ app.post('/api/contact/submit', async (req, res) => {
     }
 });
 
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
