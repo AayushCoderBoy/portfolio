a75d5fa831127167dd988b4031188be363472947
Aayush Raj <144894045+AayushCoderBoy@users.noreply.github.com>
2024-11-30 18:54:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path=require('path');
 const dotenv = require('dotenv');
 const { Pool } = require('pg');
 const nodemailer = require('nodemailer');
